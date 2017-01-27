786260b54a0014fc075648456c654e260b536aad
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-01-27 14:19:26 +0100

    [MIG] partner_changeset: Migrated to 10.0

--- partner_changeset/models/__init__.py
+++ partner_changeset/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# ?? 2015-2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_partner
 from . import res_partner_changeset
