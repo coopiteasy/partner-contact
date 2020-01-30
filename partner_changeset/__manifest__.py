dace4372698fb63a57ec67887096de197dbabbe2
houssine <houssine.bakkali@gmail.com>
2020-01-30 11:52:54 +0100

    [IMP] remove the dependency on the sale module.
    It's unnecessary to put the dependency on sale module just for having it
    in the sale menu. Instead the menu come under the contact/config

--- partner_changeset/__manifest__.py
+++ partner_changeset/__manifest__.py
@@ -1,14 +1,12 @@
-# Copyright 2015-2017 Camptocamp SA
+# Copyright 2015-2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': 'Partner Changesets',
  'version': '12.0.1.0.0',
- 'author': 'Camptocamp, Odoo Community Association (OCA)',
+ 'author': 'Camptocamp, Coop IT Easy SCRLfs, Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Sales Management',
- 'depends': ['base',
-             'sale',
-             ],
+ 'depends': ['base',],
  'website': 'http://www.camptocamp.com',
  'data': ['security/security.xml',
           'security/ir.model.access.csv',
